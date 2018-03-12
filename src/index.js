9bb8f295aa57400a710ef04deef4e3344daa45dd
Keith Rodriguez <rodriguez.keith@gmail.com>
2018-03-11 20:55:15 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './sass/style.scss';
+import App from './components/App';
+
+ReactDOM.render(<App />, document.getElementById('app'));
