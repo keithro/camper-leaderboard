93ec691b0eae4da4210b04acef2b1fb7ce340e36
Keith Rodriguez <rodriguez.keith@gmail.com>
2018-03-13 18:50:51 -0400

    Additional tweaks

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,13 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="header">
-      <h1>
+      <h1 className="header__main">
         <svg className="header__icon">
           <use xlinkHref="images/sprite.svg#icon-freecodecamp" />
         </svg>
         FreeCodeCamp
       </h1>
-      <h2>Leaderboard</h2>
+      <h2 className="header__sub">Leaderboard</h2>
     </header>
   );
 };
